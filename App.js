702397018f22923469fe89380ce0df4abf75fd2d
Seema <seema.i@peopleinteractive.in>
2018-02-14 15:40:23 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import Main from './app/components/Main'
+import {
+  View,
+  Text
+} from 'react-native'
+
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <Main/>
+    );
+  }
+}
+
